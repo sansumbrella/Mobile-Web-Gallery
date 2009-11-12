504f44b6309ddf35a94a0de2011cf3651090db6f
David Wicks <wicks.db@gmail.com>
2009-11-11 21:39:04 -0800

    added thumbnails. Removing some default styling from jqtouch.

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,22 @@
-var jQT = $.jQTouch({
+var jQT= $.jQTouch({
     icon: 'kilo.png',
     statusBar: 'black'
 });
+var galleryApp = {	currentGallery:'',
+					currentImage:'',
+					nextImage:''
+				};
 
 $(document).ready( function(){
 	//create gallery list from database
+	
+	//collect all thumbnails and put them in our list for display
+	var thumbs='';
+	var i=1;
+	for(i; i<18; i++)
+	{
+		thumbs += "<li><img src='images/t/" + i + ".jpg' alt='thumbnail'/></li>\n";
+	}
+	
+	$('#thumbs').html(thumbs);
 });
