ba8e3066b9e75f506817ca9f1a68d2c06f461d61
David Wicks <wicks.db@gmail.com>
2009-11-12 15:15:24 -0800

    made variables global so we don't confuse people with namespace issues. Will talk about the idea behind gApp, though.

--- js/gallery.js
+++ js/gallery.js
@@ -1,65 +1,31 @@
-var gApp = {currentGallery:'', zoomed:false};
-gApp.jqt = $.jQTouch({
+var currentGallery='';
+var zoomed = false;
+jqt = $.jQTouch({
     icon: 'kilo.png',
     statusBar: 'black',
 	useAnimations: true
 });
 
-gApp.Gallery = {
-	title:'Gallery',
-	current:0,
-	photos:[],
-	getNext: function() {
-		this.current = this.current+1;
-		if(this.current>=this.photos.length){
-			this.current=0;
-		}
-		console.log("getNext attempting to return " + this.current + ", " + this.photos[this.current]);
-		return this.photos[this.current];
-	},
-	getPrevious: function() {
-		this.current = this.current-1;
-		if(this.current<0){
-			this.current=this.photos.length-1 || 0;
-		}
-		console.log("getPrevious attempting to return " + this.current + ", " + this.photos[this.current]);
-		return this.photos[this.current];
-	},
-	getCurrent: function(){
-		return this.photos[this.current];
-	},
-	setCurrentFromURL: function(url){
-		for(i in this.photos)
-		{
-			if(this.photos[i].full.match(url)){
-				this.current = Number(i);
-				console.log("Current set to " + i + ", " + url);
-				return;
-			}
-		}
-	}
-};
-
 $(document).ready( function(){	
-	gApp.buildInitialGallery();
-	gApp.getUserGalleries();
+	buildInitialGallery();
+	getUserGalleries();
 	//get user gallery list from database
-	$('#mosaic').bind('pageAnimationStart', gApp.displayMosaic);
-	$('#showall').bind('click', function(e){e.preventDefault(); gApp.setGallery(gApp.allPhotos);});
-	$('#showuser').bind('click', function(e){e.preventDefault(); gApp.setGallery(gApp.userGallery);});
-	$('#previous').bind('click', function(e){e.preventDefault(); gApp.previous(); } );
-	$('#next').bind('click', function(e){e.preventDefault(); gApp.next(); });
+	$('#mosaic').bind('pageAnimationStart', displayMosaic);
+	$('#showall').bind('click', function(e){e.preventDefault(); setGallery(allPhotos);});
+	$('#showuser').bind('click', function(e){e.preventDefault(); setGallery(userGallery);});
+	$('#previous').bind('click', function(e){e.preventDefault(); previous(); } );
+	$('#next').bind('click', function(e){e.preventDefault(); next(); });
 });
 
-gApp.setGallery = function(g){
+setGallery = function(g){
 	if(g.photos === undefined){
-		g = gApp.allPhotos;
+		g = allPhotos;
 	}
-	gApp.currentGallery = g;
+	currentGallery = g;
 }
 
-gApp.displayMosaic = function(){
-	var m = gApp.currentGallery;
+displayMosaic = function(){
+	var m = currentGallery;
 	var thumbs='<ul>\n';
 	for(var p in m.photos){
 		thumbs += "<li><a href='"+m.photos[p].full+"'><img src='"+m.photos[p].thumb+"' alt='"+m.photos[p].desc+"'/></a></li>\n";
@@ -74,51 +40,51 @@ gApp.displayMosaic = function(){
 		var im = $(this).attr('href');
 		$(this).click(function(e){
 			e.preventDefault();
-			gApp.displayImage(im);
+			displayImage(im);
 		});
 	});
 };
 
-gApp.displayImage = function(im){
+displayImage = function(im){
 	$('#heroimage').attr('src',im);
 	$('#heroimage').attr('src',im);
-	gApp.currentGallery.setCurrentFromURL(im);
+	currentGallery.setCurrentFromURL(im);
 	console.log("transitioning to image " + im);
-	if(!gApp.zoomed){
-		gApp.jqt.goTo('#singleImage', 'slide');
+	if(!zoomed){
+		jqt.goTo('#singleImage', 'slide');
 		$('#singleImage').bind('pageAnimationEnd', function(){
-															gApp.zoomed=true;
+															zoomed=true;
 															$('#singleImage').unbind();
-															$('#singleImage').bind('pageAnimationEnd', function(){gApp.zoomed=false;});
+															$('#singleImage').bind('pageAnimationEnd', function(){zoomed=false;});
 															});
 	}
 };
 
-gApp.buildInitialGallery = function(){
-	gApp.allPhotos = Object.create(gApp.Gallery);
-	gApp.allPhotos.title = "All Photos";
+buildInitialGallery = function(){
+	allPhotos = Object.create(Gallery);
+	allPhotos.title = "All Photos";
 	var i=1;
 	for(i; i<18; i++)
 	{
-		gApp.allPhotos.photos[i-1] = {full:'images/'+i+'.jpg',
+		allPhotos.photos[i-1] = {full:'images/'+i+'.jpg',
 									thumb:'images/t/'+i+'.jpg',
 									desc:String(i)
 									};
 	}
 };
 
-gApp.previous = function(){
+previous = function(){
 	this.displayImage(this.currentGallery.getPrevious().full);
 };
 
-gApp.next = function(){
+next = function(){
 	this.displayImage(this.currentGallery.getNext().full);
 };
 
-gApp.getUserGalleries = function(){
-	gApp.userGallery = Object.create(gApp.Gallery);
-	gApp.userGallery.title = "User Gallery";
-	gApp.userGallery.photos = [
+getUserGalleries = function(){
+	userGallery = Object.create(Gallery);
+	userGallery.title = "User Gallery";
+	userGallery.photos = [
 		{full:'images/1.jpg',
 		thumb:'images/t/1.jpg',
 		desc:'Photo 1'},
@@ -134,6 +100,6 @@ gApp.getUserGalleries = function(){
 	]
 };
 
-gApp.updateGallery = function(){
+updateGallery = function(){
 	
 };
--- js/util.js
+++ js/util.js
@@ -5,3 +5,38 @@ if(typeof Object.create !== 'function'){
 		return new F();
 	};
 };
+
+Gallery = {
+	title:'Gallery',
+	current:0,
+	photos:[],
+	getNext: function() {
+		this.current = this.current+1;
+		if(this.current>=this.photos.length){
+			this.current=0;
+		}
+		console.log("getNext attempting to return " + this.current + ", " + this.photos[this.current]);
+		return this.photos[this.current];
+	},
+	getPrevious: function() {
+		this.current = this.current-1;
+		if(this.current<0){
+			this.current=this.photos.length-1 || 0;
+		}
+		console.log("getPrevious attempting to return " + this.current + ", " + this.photos[this.current]);
+		return this.photos[this.current];
+	},
+	getCurrent: function(){
+		return this.photos[this.current];
+	},
+	setCurrentFromURL: function(url){
+		for(i in this.photos)
+		{
+			if(this.photos[i].full.match(url)){
+				this.current = Number(i);
+				console.log("Current set to " + i + ", " + url);
+				return;
+			}
+		}
+	}
+};
