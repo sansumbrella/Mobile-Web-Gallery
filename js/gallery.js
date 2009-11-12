74f3d356a335c86f50d492a0e69f4766358ffe32
David Wicks <wicks.db@gmail.com>
2009-11-11 23:56:01 -0800

    image count css adjusted. Initial mosaic build triggered if no photo set is defined.

--- js/gallery.js
+++ js/gallery.js
@@ -5,32 +5,23 @@ gApp.jqt = $.jQTouch({
 });
 
 $(document).ready( function(){	
-	//Create collection of all photos
-	gApp.allPhotos = {	title:'All Photos',
-							photos:[]
-						};
-	var i=1;
-	for(i; i<18; i++)
-	{
-		gApp.allPhotos.photos[i] = {	full:'images/'+i+'.jpg',
-										thumb:'images/t/'+i+'.jpg',
-										desc:i.toString()
-										};
-	}
+	gApp.buildInitialCollection();
 	//get user gallery list from database
-	
-	//navigate to the mosaic view
-	gApp.displayMosaic(gApp.allPhotos);
-	// gApp.jqt.goTo('#mosaic', 'slide');
+	$('#mosaic').bind('pageAnimationStart', gApp.displayMosaic);
 });
 
 gApp.displayMosaic = function(m){
+	if(m.photos === undefined){
+		m = gApp.allPhotos;
+	}
 	var thumbs='<ul>\n';
 	for(var p in m.photos){
 		thumbs += "<li><a href='"+m.photos[p].full+"'><img src='"+m.photos[p].thumb+"' alt='"+m.photos[p].desc+"'/></a></li>\n";
 	}
 	thumbs+="</ul>\n";
 	thumbs+="<h2 class='count'>"+m.photos.length+" Photos</h2>";
+	
+	$('#mosaic .toolbar h1').text(m.title);
 	$('#thumbs').html(thumbs);
 	//change link behavior
 	$('#thumbs a').each( function(){
@@ -46,3 +37,18 @@ gApp.displayImage = function(im){
 	$('#heroimage').attr('src',im);
 	gApp.jqt.goTo('#singleImage', 'slide');
 };
+
+gApp.buildInitialCollection = function(){
+	gApp.allPhotos = {	title:'All Photos',
+						photos:[]
+						};
+	var i=1;
+	for(i; i<18; i++)
+	{
+		gApp.allPhotos.photos[i] = {full:'images/'+i+'.jpg',
+									thumb:'images/t/'+i+'.jpg',
+									desc:i.toString()
+									};
+	}
+	gApp.currentGallery = gApp.allPhotos;
+}
