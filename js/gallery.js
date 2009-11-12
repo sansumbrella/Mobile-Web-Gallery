752b261404ce8d53bcac48c10bccc1fec9e409c5
David Wicks <wicks.db@gmail.com>
2009-11-11 23:30:54 -0800

    thumbnail click sends to full image within context of page.

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,48 @@
-var jQT= $.jQTouch({
+var gApp = {};
+gApp.jqt = $.jQTouch({
     icon: 'kilo.png',
     statusBar: 'black'
 });
-var galleryApp = {	currentGallery:'',
-					currentImage:'',
-					nextImage:''
-				};
 
-$(document).ready( function(){
-	//create gallery list from database
-	
-	//collect all thumbnails and put them in our list for display
-	var thumbs='';
+$(document).ready( function(){	
+	//Create collection of all photos
+	gApp.allPhotos = {	title:'All Photos',
+							photos:[]
+						};
 	var i=1;
 	for(i; i<18; i++)
 	{
-		thumbs += "<li><img src='images/t/" + i + ".jpg' alt='thumbnail'/></li>\n";
+		gApp.allPhotos.photos[i] = {	full:'images/'+i+'.jpg',
+										thumb:'images/t/'+i+'.jpg',
+										desc:i.toString()
+										};
 	}
+	//get user gallery list from database
 	
-	$('#thumbs').html(thumbs);
+	//navigate to the mosaic view
+	gApp.displayMosaic(gApp.allPhotos);
+	// gApp.jqt.goTo('#mosaic', 'slide');
 });
+
+gApp.displayMosaic = function(m){
+	var thumbs='<ul>\n';
+	for(var p in m.photos){
+		thumbs += "<li><a href='"+m.photos[p].full+"'><img src='"+m.photos[p].thumb+"' alt='"+m.photos[p].desc+"'/></a></li>\n";
+	}
+	thumbs+="</ul>\n";
+	thumbs+="<h2 class='count'>"+m.photos.length+" Photos</h2>";
+	$('#thumbs').html(thumbs);
+	//change link behavior
+	$('#thumbs a').each( function(){
+		var im = $(this).attr('href');
+		$(this).click(function(e){
+			e.preventDefault();
+			gApp.displayImage(im);
+		});
+	});
+};
+
+gApp.displayImage = function(im){
+	$('#heroimage').attr('src',im);
+	gApp.jqt.goTo('#singleImage', 'slide');
+};
