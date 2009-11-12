54cd8eb9755c1daf5c42bf737256e7549aa29ae9
David Wicks <wicks.db@gmail.com>
2009-11-12 08:52:57 -0800

    placeholder user gallery tested to see that swapping image sets works.

--- js/gallery.js
+++ js/gallery.js
@@ -45,15 +45,21 @@ $(document).ready( function(){
 	gApp.getUserGalleries();
 	//get user gallery list from database
 	$('#mosaic').bind('pageAnimationStart', gApp.displayMosaic);
+	$('#showall').bind('click', function(e){e.preventDefault(); gApp.setGallery(gApp.allPhotos);});
+	$('#showuser').bind('click', function(e){e.preventDefault(); gApp.setGallery(gApp.userGallery);});
 	$('#previous').bind('click', function(e){e.preventDefault(); gApp.previous(); } );
 	$('#next').bind('click', function(e){e.preventDefault(); gApp.next(); });
 });
 
-gApp.displayMosaic = function(m){
-	if(m.photos === undefined){
-		m = gApp.allPhotos;
+gApp.setGallery = function(g){
+	if(g.photos === undefined){
+		g = gApp.allPhotos;
 	}
-	gApp.currentGallery = m;
+	gApp.currentGallery = g;
+}
+
+gApp.displayMosaic = function(){
+	var m = gApp.currentGallery;
 	var thumbs='<ul>\n';
 	for(var p in m.photos){
 		thumbs += "<li><a href='"+m.photos[p].full+"'><img src='"+m.photos[p].thumb+"' alt='"+m.photos[p].desc+"'/></a></li>\n";
@@ -112,6 +118,20 @@ gApp.next = function(){
 gApp.getUserGalleries = function(){
 	gApp.userGallery = Object.create(gApp.Gallery);
 	gApp.userGallery.title = "User Gallery";
+	gApp.userGallery.photos = [
+		{full:'images/1.jpg',
+		thumb:'images/t/1.jpg',
+		desc:'Photo 1'},
+		{full:'images/4.jpg',
+		thumb:'images/t/4.jpg',
+		desc:'Photo 2'},
+		{full:'images/2.jpg',
+		thumb:'images/t/2.jpg',
+		desc:'Photo 3'},
+		{full:'images/5.jpg',
+		thumb:'images/t/5.jpg',
+		desc:'Photo 4'}
+	]
 };
 
 gApp.updateGallery = function(){
